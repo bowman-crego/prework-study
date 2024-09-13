a31fee39de864bebebd1363f7fdcbb8af6804815
Bowman Crego <bowmanccrego@gmail.com>
2024-09-13 09:48:34 -0700

    added a for loop to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,19 @@
-const topics = ['HTML','CSS','Git',"JavaScript"];
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (let x = 0; x < topics.length; x++) {
+for (let x=0; x < topics.length; x++) {
     console.log(topics[x]);
 }
+
 /*
-if (topics === 'HTML') {
+if (topic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
+} else if (topic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topics === 'Git') {
+} else if (topic === 'Git') {
   console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
+} else if (topic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
- */
+  */
